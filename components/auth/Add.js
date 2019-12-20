7a3123c9593c66d98c3d507fec557189e7975861
Jose Prieto <jose.prieto@3eengenharia.com.br>
2019-12-20 17:06:15 -0300

    wallpaper add

--- components/auth/Add.js
+++ components/auth/Add.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
+import { View, StyleSheet, Text, Button,ImageBackground } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 class Add extends React.Component {
-  
   render() {
     const { navigation } = this.props;
+    const img = "https://res.cloudinary.com/dvm6sgg1h/image/upload/v1576845497/jsqehywsp3ykewvol2dj.jpg";
     return (
-      <View style={styles.container}>
-         <Button 
-        title='Select Photo'
-        onPress={()=>{navigation.navigate('Selection')}}
-        />
-         <Text style={{color: 'white'}}></Text>
-         <Button 
-        title='Take a Photo'
-        onPress={()=>{navigation.navigate('Selection')}}
-        />
+      <View style = { styles.container } >
+      <ImageBackground  style= { styles.backgroundImage } source={{ uri: img }}>
+      <View style= { styles.logoContainer }>
+        <Text style = { styles.logoDescription }>
+          Pick Uphoto!
+        </Text>
+        <Ionicons styles={styles.icon} onPress={()=>{navigation.navigate('Selection')}} name="ios-aperture" size={100} color="#2196f3" />       
+      </View>
+      </ImageBackground>
       </View>
     );
-  }
+  } 
 }
 
 const styles = StyleSheet.create({
@@ -26,8 +26,31 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#43CBFF',
-  }
+  },
+  icon: {
+    alignItems: "center",
+  },
+  logoContainer:{
+    alignItems: "center",
+  },
+  logoText: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: 'white'
+  },
+  logoDescription:{
+    fontSize: 35,
+    fontWeight: '600',
+    color: 'white'
+  },
+  backgroundImage:{
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    justifyContent: "center",
+    alignItems: "center",
+    opacity: 1
+},
 });
 
 export default Add;
